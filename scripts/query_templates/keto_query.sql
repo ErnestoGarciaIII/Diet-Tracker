c645d25981c3e106f0615871d82459a66a7228de
Lid <berkscott998@outlook.com>
2026-03-31 14:09:02 -0500

    Bro idk

--- scripts/query_templates/keto_query.sql
+++ scripts/query_templates/keto_query.sql
@@ -2,6 +2,6 @@ SELECT fdc_id
 FROM food_nutrient
 GROUP BY fdc_id
 HAVING (
-    MAX(CASE WHEN nutrient_id = 1005 THEN amount ELSE 0 END) - 
-    MAX(CASE WHEN nutrient_id = 1079 THEN amount ELSE 0 END)
+    COALESCE(MAX(CASE WHEN nutrient_id = 1005 THEN amount ELSE 0 END), 0) - 
+    COALESCE(MAX(CASE WHEN nutrient_id = 1079 THEN amount ELSE 0 END), 0)
 ) <= 1.0
