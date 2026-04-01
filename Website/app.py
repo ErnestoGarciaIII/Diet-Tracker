d1c8777bcc013e10fc92d8ec4be9e8f92fa76f56
Jayce Pitman <Jayce.Pitman@Livanova.com>
2026-04-01 06:50:42 -0500

    fixed undefined DB_PATH variable in app.py

--- Website/app.py
+++ Website/app.py
@@ -429,7 +429,7 @@ def get_nutrients():
     params = [fdc_id] + NUTRIENT_IDS
  
     try:
-        conn = sqlite3.connect(DB_PATH)
+        conn = connect_to_database();
         conn.row_factory = sqlite3.Row
         cur  = conn.cursor()
         cur.execute(sql, params)
