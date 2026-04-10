a90f9738b819c9334bc27a486d04754682316deb
Edgar <edgarpadilla0627@gmail.com>
2026-04-09 19:35:12 -0500

    updated app.py

--- Website/app.py
+++ Website/app.py
@@ -406,9 +406,43 @@ NUTRIENT_IDS = [
     1099, 1100, 1238, 1090, 1101, 1102, 1091, 1092, 1103, 1093, 1095
 ]
 
-@app.route('/api/apply-filter', methods=['GET'])
+@app.route('/api/apply-filter', methods=['POST'])
 def apply_that_filter():
-    return 0;
+    data = request.get_json()
+    try:
+        user_id = data.get('user_id')
+        restriction = data.get('restriction')
+        
+        if not restriction:
+            return jsonify({'error': 'Restriction is required to set a filter'}), 400
+
+        if not user_id:
+            return jsonify({'error': 'User ID required to maintain session'}), 400
+
+        if user_id not in active_user_conns:
+            active_user_conns[user_id] = connectDB()
+
+        conn = active_user_conns[user_id]
+        cur = conn.cursor()
+
+        cur.execute("""
+            SELECT restrictionId
+            FROM Restrictions
+            WHERE name = ?
+        """, (restriction,))
+        
+        restrictionId = cur.fetchone()
+        print(f"Applying filter: ${restriction} RestrictionId: ${restrictionId[0]}")
+
+        apply_filter(restrictionId[0], conn)
+
+        return jsonify({
+            'message': 'Filter set successfully',
+            'filterId': restrictionId
+        }), 201
+    except Exception as e:
+        print("[ERROR]: ", e)
+        return jsonify({'error': str(e)}), 400
 
 
 @app.route('/api/search-engine', methods=['GET'])
