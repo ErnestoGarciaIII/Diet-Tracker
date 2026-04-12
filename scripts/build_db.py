4dbfcfc28e250136a879520d2aaeed0957e5f446
BumblinBee <berkscott998@outlook.com>
2026-04-12 02:13:03 -0500

    Added that ONE FUCKING LINE of account creation date :) to build_db :)

--- scripts/build_db.py
+++ scripts/build_db.py
@@ -83,7 +83,7 @@ def build_users_table(conn, cursor):
         reset_token TEXT,
         reset_token_expiry TEXT,
         date_of_birth TEXT,
-        age INTEGER,
+        account_creation_date TEXT,
         sex TEXT,
         height_inches INTEGER,
         weight_lbs INTEGER,
